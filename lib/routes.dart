c7ee9db0dca06e16d70734037ab8ac834d94b43b
todayama_r <13657682+Corvus400@users.noreply.github.com>
2021-12-01 18:22:03 +0900

    :sparkles: 明示的アニメーション用の画面を追加

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'contents/implicit_animations_1.dart';
 import 'contents/implicit_animations_2.dart';
+import 'contents/explicit_animations_1.dart';
 
 const kRouteImplicitAnimations1 = "/implicit_animations_1";
 const kRouteImplicitAnimations2 = "/implicit_animations_2";
+const kRouteExplicitAnimations1 = "/explicit_animations_1";
 
 final kRoutes = <String, WidgetBuilder>{
   kRouteImplicitAnimations1: (BuildContext context) => ImplicitAnimations1(),
   kRouteImplicitAnimations2: (BuildContext context) => ImplicitAnimations2(),
+  kRouteExplicitAnimations1: (BuildContext context) => ExplicitAnimations1(),
 };
