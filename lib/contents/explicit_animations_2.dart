b66ecf8b1bd2fb71ca79ec6063c7ad076c4a299a
todayama_r <13657682+Corvus400@users.noreply.github.com>
2021-12-01 18:47:12 +0900

    :sparkles: AnimatedWidgetを追加 追加したAnimatedWidgetを追加した画面で使用

--- lib/contents/explicit_animations_2.dart
+++ lib/contents/explicit_animations_2.dart
@@ -1,3 +1,4 @@
+import 'package:animation_tutorials/animated_widget/AnimatedLogo.dart';
 import 'package:flutter/material.dart';
 
 class ExplicitAnimations2 extends StatefulWidget {
@@ -7,14 +8,39 @@ class ExplicitAnimations2 extends StatefulWidget {
   _ExplicitAnimations2State createState() => _ExplicitAnimations2State();
 }
 
-class _ExplicitAnimations2State extends State<ExplicitAnimations2> {
+class _ExplicitAnimations2State extends State<ExplicitAnimations2> with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(seconds: 2),
+        vsync: this
+    );
+    _animation = Tween<double>(
+        begin: 0,
+        end: 300
+    ).animate(_controller);
+    _controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Explicit Animations 2'),
+        title: const Text('Explicit Animations 1'),
       ),
-      body: Container(),
+      body: Center(
+        child: AnimatedLogo(animation: _animation),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
